c5ade3ea250a564589cae9154bbac74168134dda
duyphongdn97 <duyphongdn1997@gmail.com>
2021-06-07 11:19:35 +0700

    apply detect for abnormal images

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import base64
 from TwoClassClassifier import predict, img_to_base64_str
 import base64
 from io import BytesIO
-
+import cv2
+from yolov5.utils.general import convert_from_image_to_cv2
+from detect import detect
 
 app = Flask(__name__)
 app.config['TESTING'] = True
@@ -24,22 +26,29 @@ def hello_world():
             #read image file string data
             filestr = file.read()
             #convert string data to numpy array
-            npimg = np.fromstring(filestr, np.uint8)
+            npimg = np.frombuffer(filestr, np.uint8) #change fromstring --> frombuffer
             # convert numpy array to image
-
-            # img = cv2.imdecode(npimg,cv2.IMREAD_COLOR) # Using cv2 library
-            img = Image.fromarray(npimg).convert('RGB') # Using PIL library
-            probabilities = 0
-            print('Image have been uploaded!')
+            opencvImage = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
+            img = Image.fromarray(npimg).convert('RGB') # Using PIL library because input of EffNet need a PIL format
             name, probabilities = predict(model_name='efficientnet_b2_pruned', weights_path="model_step1.pth", image = img)
 
-            #base64 encoding for displaying image on webpage
+            if name == 'normal' :
+                #base64 encoding for displaying image on webpage
+                print('name = ', name, '\t', 'probabilities = ', probabilities)
+                retval, buffer_img= cv2.imencode('.png', opencvImage)
+
+                data = base64.b64encode(buffer_img).decode('utf-8')
+                return render_template('result.html', imagebase64=data, name=name, probability=probabilities)
+            else :
+                image_pred, names = detect(opencvImage, weights="best.pt", img_size=640)
+                #base64 encoding for displaying image on webpage
+                for i in names:
+                    print("-->", i)
+                retval, buffer_img= cv2.imencode('.png', image_pred)
+
+                data = base64.b64encode(buffer_img).decode('utf-8')
+                return render_template('result.html', imagebase64=data, name=name, probability=probabilities)
             
-            # img = img_to_base64_str(img)
-            print('name = ', name, '\t', 'probabilities = ', probabilities)
-            # retval, buffer_img= cv2.imencode('.jpg', img)
-            # data = base64.b64encode(img).decode('utf-8')
-            return render_template('result.html', imagebase64=img, name=name, probability=probabilities)
             
 
 if __name__ == '__main__':
--- yolov5/utils/general.py
+++ yolov5/utils/general.py
@@ -33,6 +33,19 @@ cv2.setNumThreads(0)  # prevent OpenCV from multithreading (incompatible with Py
 os.environ['NUMEXPR_MAX_THREADS'] = str(min(os.cpu_count(), 8))  # NumExpr max threads
 
 
+import numpy as np
+from PIL import Image
+
+
+def convert_from_cv2_to_image(img: np.ndarray) -> Image:
+    # return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    return Image.fromarray(img)
+
+
+def convert_from_image_to_cv2(img: Image) -> np.ndarray:
+    return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    # return np.asarray(img)
+
 def set_logging(rank=-1, verbose=True):
     logging.basicConfig(
         format="%(message)s",
@@ -64,6 +77,40 @@ def is_docker():
 #         return True
 #     except Exception as e:
 #         return False
+
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True):
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - \
+        new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, 32), np.mod(dh, 32)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / \
+            shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(
+        img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
 
 
 def emojis(str=''):
